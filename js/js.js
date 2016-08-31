239cc095a184faca7f5d105b8f1d65fcfb9fc7f6
Nevendra Seonarain <nevendra@live.com>
2016-08-31 15:49:30 -0400

    check flip image

--- js/js.js
+++ js/js.js
@@ -1,13 +1,35 @@
 $(document).ready(function(){
 	
- $('.flip').click(function(){
-        if($(this).find('.card').hasClass('flipped')){
-        	$(this).find('.card').removeClass('flipped');
-        } else {
-        	$(this).find('.card').addClass('flipped');
-        }
- });
+ // $('.flip').click(function(){
+ //        if($(this).find('.card').hasClass('flipped')){
+ //        	$(this).find('.card').removeClass('flipped');
+ //        } else {
+ //        	$(this).find('.card').addClass('flipped');
+ //        }
+ // });
 
+
+
+ //  $('#squaredTwo').click(function(){
+ //        if($(this).find('.card').hasClass('flipped')){
+ //        	$(this).find('.card').removeClass('flipped');
+ //        } else {
+ //        	$(this).find('.card').addClass('flipped');
+ //        }
+ // });
+
+var ckbox = $('#squaredTwo');
+
+$('input').on('click',function () {
+		var box = $(this).parent().parent().parent();
+        if (ckbox.is(':checked')) {
+	        if(box.find('.card').hasClass('flipped')){
+	        	box.find('.card').removeClass('flipped');
+	        } else {
+	        	box.find('.card').addClass('flipped');
+	        }
+        }
+    });
 
 });
 
