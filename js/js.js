0d1e2fb8b286fdee83d9eabfc760ce37f45e009a
Nevendra <nevendra@live>
2016-08-31 16:41:55 -0400

    Updating gh-pages from master branch via GitBot

--- js/js.js
+++ js/js.js
@@ -1,12 +1,37 @@
 $(document).ready(function(){
 	
- $('.flip').click(function(){
-        if($(this).find('.card').hasClass('flipped')){
-        	$(this).find('.card').removeClass('flipped');
-        } else {
-        	$(this).find('.card').addClass('flipped');
+ // $('.flip').click(function(){
+ //        if($(this).find('.card').hasClass('flipped')){
+ //        	$(this).find('.card').removeClass('flipped');
+ //        } else {
+ //        	$(this).find('.card').addClass('flipped');
+ //        }
+ // });
+
+
+
+ //  $('#squaredTwo').click(function(){
+ //        if($(this).find('.card').hasClass('flipped')){
+ //        	$(this).find('.card').removeClass('flipped');
+ //        } else {
+ //        	$(this).find('.card').addClass('flipped');
+ //        }
+ // });
+
+var ckbox = $('#squaredTwo');
+
+$('input').on('click',function () {
+		var box = $(this).parent().parent().parent();
+        if ($(this).is(':checked') && $(this).hasClass('flipBox')) {
+	        if(box.find('.card').hasClass('flipped')){
+	        	box.find('.card').removeClass('flipped');
+	        } else {
+	        	box.find('.card').addClass('flipped');
+	        	$(this).attr('disabled', true)
+	        }
         }
- });
+    });
+
 
 
 });
