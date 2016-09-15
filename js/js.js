42f942fe32f96b59b907f6679905695c519406d3
Nevendra <nevendra@live>
2016-09-15 17:01:23 -0400

    Updating gh-pages from master branch via GitBot

--- js/js.js
+++ js/js.js
@@ -1,70 +1,16 @@
 $(document).ready(function(){
 	
- // $('.flip').click(function(){
- //        if($(this).find('.card').hasClass('flipped')){
- //        	$(this).find('.card').removeClass('flipped');
- //        } else {
- //        	$(this).find('.card').addClass('flipped');
- //        }
- // });
-
-
-
- //  $('#squaredTwo').click(function(){
- //        if($(this).find('.card').hasClass('flipped')){
- //        	$(this).find('.card').removeClass('flipped');
- //        } else {
- //        	$(this).find('.card').addClass('flipped');
- //        }
- // });
-
 var ckbox = $('#squaredTwo');
 
-$('input').on('click',function () {
-		var box = $(this).parent().parent().parent();
-        if ($(this).is(':checked') && $(this).hasClass('flipBox')) {
-	        if(box.find('.card').hasClass('flipped')){
-	        	box.find('.card').removeClass('flipped');
-	        } else {
-	        	box.find('.card').addClass('flipped');
-	        	$(this).attr('disabled', true)
-	        }
-        }
-    });
-
-// $('input').on('click',function () {
-// 		var box = $(this).parent().parent().parent();
-//         if ($(this).is(':checked') && $(this).hasClass('slideBox')) {
-// 	        if(box.find('.imgGallery').hasClass('slideImg')){
-// 	        	box.find('.imgGallery').removeClass('slideImg');
-// 	        } else {
-// 	        	box.find('.imgGallery').addClass('slideImg');
-// 	        	if(box.find('.imgGallery').hasClass('slideImg')) {
-// 	        		$(box.find('.imgGallery')).animate({height: '700px'}, 'slow')
-// 	        		$(box.find('.slide')).animate({height: '800px'}, 'slow', showImgGallery(box));
-// 	        	}
-// 	        	$(this).attr('disabled', true)
-// 	        }
-//         }
-//     });
 
 $('input').on('click',function () {
 		var box = $(this).parent().parent().parent().parent();
-		$(box.find('.slide')).slideToggle("slow", showImgGallery(box));
-		$(this).attr('disabled', true)
-		console.log("hey");
-        // if ($(this).is(':checked') && $(this).hasClass('slideBox')) {
-	       //  if(box.find('.imgGallery').hasClass('slideImg')){
-	       //  	box.find('.imgGallery').removeClass('slideImg');
-	       //  } else {
-	       //  	box.find('.imgDisplay').addClass('slideImg');
-	       //  	if(box.find('.imgGallery').hasClass('slideImg')) {
-	       //  		// $(box.find('.imgGallery')).animate({height: '700px'}, 'slow')
-	       //  		$(box.find('.slide')).animate({height: '800px'}, 'slow', showImgGallery(box));
-	       //  	}
-	       //  	$(this).attr('disabled', true)
-	       //  }
-        // }
+		if($(this).hasClass('slideBox')){
+			$(box.find('.slide')).slideToggle("slow", showImgGallery(box));
+			$(this).attr('disabled', true)
+		} else if ($(this).hasClass('flipBox')){
+			box.find('.card').addClass('flipped')
+		}
     });
 
 function showImgGallery(box) {
